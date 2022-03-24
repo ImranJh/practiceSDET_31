9036d5483c2890efec0d836a34cb824907786a52
Hp <Hp@DESKTOP-5E614SC>
2022-03-24 17:20:11 +0530

    Advertisement class added

---
+++ src/test/java/com/crm/SDET_31Practice/CreateAdvertisementTest.java
@@ -0,0 +1,12 @@
+package com.crm.SDET_31Practice;
+
+import org.testng.annotations.Test;
+
+public class CreateAdvertisementTest {
+	@Test
+	public void Advertisement()
+	{
+		System.out.println("add created");
+	}
+
+}
