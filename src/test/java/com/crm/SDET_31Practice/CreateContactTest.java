ec165296cdbc07d244e76f8e699b6230e685f1a1
Hp <Hp@DESKTOP-5E614SC>
2022-03-24 14:25:05 +0530

    Contact Updated by Imran

---
+++ src/test/java/com/crm/SDET_31Practice/CreateContactTest.java
@@ -0,0 +1,12 @@
+package com.crm.SDET_31Practice;
+
+import org.testng.annotations.Test;
+
+public class CreateContactTest {
+	@Test
+	public void Contact()
+	{
+		System.out.println("Contact Updated");
+	}
+
+}
