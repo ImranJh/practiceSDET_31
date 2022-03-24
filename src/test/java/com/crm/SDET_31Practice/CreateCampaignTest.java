93e1761f3d5f629a059e9f8731bc94cdb83c9936
Hp <Hp@DESKTOP-5E614SC>
2022-03-24 14:54:56 +0530

    Campaign code added

---
+++ src/test/java/com/crm/SDET_31Practice/CreateCampaignTest.java
@@ -0,0 +1,18 @@
+package com.crm.SDET_31Practice;
+
+import org.testng.annotations.Test;
+
+public class CreateCampaignTest {
+	@Test
+	public void Campaign()
+	{
+		System.out.println("U");
+		System.out.println("P");
+		System.out.println("D");
+		System.out.println("A");
+		System.out.println("T");
+		System.out.println("E");
+		System.out.println("D");
+	}
+
+}
